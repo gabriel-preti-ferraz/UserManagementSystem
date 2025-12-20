86ee147b2a80d1d9e989d78d4a18c1dcb26110c7
Gabriel Preti Ferraz <gferraz381@gmail.com>
2025-12-20 15:25:08 -0300

    feat: user role in database

--- backend/database.sql
+++ backend/database.sql
@@ -1,9 +1,12 @@
 DROP TABLE IF EXISTS users;
 
+CREATE TYPE user_role AS ENUM ('user', 'admin')
+
 CREATE TABLE users (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     username VARCHAR(255) NOT NULL UNIQUE,
     email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    role user_role default 'user' NOT NULL
 );
 
