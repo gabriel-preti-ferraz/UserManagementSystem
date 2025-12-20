a0eb2957422b9cbf2dea95b2eaee1679fade7c17
Gabriel Preti Ferraz <gferraz381@gmail.com>
2025-12-19 21:15:50 -0300

    refactor: ForgotPass page renamed to PassRecovery

--- frontend/src/pages/ForgotPass.jsx
+++
@@ -1,7 +0,0 @@
-import "../css/ForgotPass.css"
-
-function ForgotPass() {
-    return (
-        <div>Hello World</div>
-    )
-}
---
+++ frontend/src/pages/PassRecovery.jsx
@@ -0,0 +1,9 @@
+import "../css/PassRecovery.css"
+
+function PassRecovery() {
+    return (
+        <div>Hello World</div>
+    )
+}
+
+export default PassRecovery
