601d56c72e3d79ff84eedde789a262a89c5a0eaa
Gabriel Preti Ferraz <gferraz381@gmail.com>
2025-12-20 12:13:28 -0300

    feat: .env

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,12 @@
 import { Client } from 'pg'
+import dotenv from 'dotenv'
+dotenv.config({path: "./.env"})
 
 const client = new Client({
-    host: "localhost",
-    user: "postgres",
-    port: "5432",
-    password: "admin",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     database: "UserManagementSystem"
 })
 
