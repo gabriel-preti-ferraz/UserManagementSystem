a2a5afd9b2707b360c5a0a34f7c62c1691475318
Gabriel Preti Ferraz <gferraz381@gmail.com>
2025-12-22 18:14:23 -0300

    feat: UserCard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -17,7 +17,15 @@ function Dashboard() {
             </div>
             
             <div className="users-field">
-                
+                <div className="user-card">
+                    <div className="user-photo">
+                        <img src=""/>
+                    </div>
+                    <div className="user-info">
+                        <h2>Username</h2>
+                        <p>email</p>
+                    </div>
+                </div>
             </div>
         </Wrapper>
     )
